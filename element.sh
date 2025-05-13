76e5bf0fba65ffdda2248bd14d81233c2c16240f
ausi <mntskeaese@gmail.com>
2025-05-14 00:00:06 +0200

    Build a Periodic Table Database

---
+++ element.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# This script queries the periodic table database
+
+#!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+# Check if argument was passed
+if [[ -z $1 ]]; then
+  echo "Please provide an element as an argument."
+  exit
+fi
+
+# Determine the query condition
+if [[ $1 =~ ^[0-9]+$ ]]; then
+  CONDITION="atomic_number=$1"
+elif [[ $1 =~ ^[A-Z][a-z]?$ ]]; then
+  CONDITION="symbol='$1'"
+else
+  CONDITION="name='$1'"
+fi
+
+# Fetch the data
+RESULT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE $CONDITION")
+
+# Check if result is empty
+if [[ -z $RESULT ]]; then
+  echo "I could not find that element in the database."
+  exit
+fi
+
+# Parse and print result
+IFS="|" read -r ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELT BOIL <<< "$RESULT"
+echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
